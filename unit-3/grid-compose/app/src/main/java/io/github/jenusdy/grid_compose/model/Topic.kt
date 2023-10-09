afc6f455fcf96affd5f55cd49e60fbc00d9ba0db
Jenusdy <jenusdy@gmail.com>
2023-10-10 03:32:18 +0700

    Add project Lazy Column Width Grid 2

---
+++ unit-3/grid-compose/app/src/main/java/io/github/jenusdy/grid_compose/model/Topic.kt
@@ -0,0 +1,10 @@
+package io.github.jenusdy.grid_compose.model
+
+import androidx.annotation.DrawableRes
+import androidx.annotation.StringRes
+
+data class Topic(
+    @StringRes val name: Int,
+    val availableCourses: Int,
+    @DrawableRes val imageRes: Int
+)
