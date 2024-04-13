9ec6992dcb56cfbd43e1f292552799ede3bfc227
Fawcet Jenusdy Makay <jenusdy@gmail.com>
2024-04-13 19:31:30 +0700

    Add Image into Card

--- unit-4/jakarta-city/app/src/main/java/io/github/jenusdy/jakartacity/ui/JakartaCityApp.kt
+++ unit-4/jakarta-city/app/src/main/java/io/github/jenusdy/jakartacity/ui/JakartaCityApp.kt
@@ -1,13 +1,17 @@
 package io.github.jenusdy.jakartacity.ui
 
+import androidx.annotation.DrawableRes
+import androidx.compose.foundation.Image
 import androidx.compose.foundation.background
 import androidx.compose.foundation.layout.Box
 import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.Row
+import androidx.compose.foundation.layout.Spacer
 import androidx.compose.foundation.layout.fillMaxHeight
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.layout.width
 import androidx.compose.foundation.lazy.LazyColumn
 import androidx.compose.foundation.lazy.items
 import androidx.compose.material.icons.Icons
@@ -29,13 +33,16 @@ import androidx.compose.material3.Surface
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.runtime.collectAsState
+import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.graphics.vector.ImageVector
 import androidx.compose.ui.platform.testTag
 import androidx.compose.ui.res.dimensionResource
+import androidx.compose.ui.res.painterResource
 import androidx.compose.ui.res.stringResource
 import androidx.compose.ui.text.style.TextOverflow
 import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.unit.dp
 import androidx.lifecycle.viewmodel.compose.viewModel
 import io.github.jenusdy.jakartacity.R
 import io.github.jenusdy.jakartacity.data.DestinationType
@@ -131,17 +138,19 @@ private fun DestinationListItem(
     modifier: Modifier = Modifier
 ) {
     Card(
-        modifier = modifier,
-        colors = CardDefaults.cardColors(
-            MaterialTheme.colorScheme.primaryContainer
-        ),
+        modifier = modifier
+            .padding(dimensionResource(R.dimen.destination_list_item_vertical_spacing)),
         onClick = onCardClick
     ) {
         Column(
             modifier = Modifier
                 .fillMaxWidth()
-                .padding(dimensionResource(R.dimen.destination_list_item_inner_padding))
+                .padding(dimensionResource(R.dimen.destination_list_item_inner_padding)),
         ) {
+            Image(
+                painter = painterResource(id = destination.image),
+                contentDescription = null
+            )
             Text(
                 text = stringResource(destination.title),
                 style = MaterialTheme.typography.bodyLarge,
@@ -151,13 +160,22 @@ private fun DestinationListItem(
                     bottom = dimensionResource(R.dimen.destination_list_item_subject_body_spacing)
                 ),
             )
-            Text(
-                text = stringResource(destination.title),
-                style = MaterialTheme.typography.bodyMedium,
-                maxLines = 2,
-                color = MaterialTheme.colorScheme.onSurfaceVariant,
-                overflow = TextOverflow.Ellipsis
-            )
+            Row(
+                modifier = Modifier.fillMaxWidth()
+            ){
+                Text(
+                    text = stringResource(destination.title),
+                    style = MaterialTheme.typography.bodyMedium,
+                    color = MaterialTheme.colorScheme.onSurfaceVariant,
+                    modifier = modifier.weight(1f)
+                )
+                Spacer(Modifier.weight(1f))
+                Text(
+                    text = "Rating " + destination.rating.toString(),
+                    style = MaterialTheme.typography.bodyMedium,
+                    color = MaterialTheme.colorScheme.onSurfaceVariant,
+                )
+            }
         }
     }
 }
