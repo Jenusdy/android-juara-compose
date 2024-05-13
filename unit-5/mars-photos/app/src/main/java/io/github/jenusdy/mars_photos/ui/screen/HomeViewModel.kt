870c3faee5108132c4924113cc03948ac4f1552a
Fawcet Jenusdy Makay <jenusdy@gmail.com>
2024-05-13 23:04:41 +0700

    Add Retrofit

--- unit-5/mars-photos/app/src/main/java/io/github/jenusdy/mars_photos/ui/screen/HomeViewModel.kt
+++ unit-5/mars-photos/app/src/main/java/io/github/jenusdy/mars_photos/ui/screen/HomeViewModel.kt
@@ -4,18 +4,35 @@ import androidx.compose.runtime.getValue
 import androidx.compose.runtime.mutableStateOf
 import androidx.compose.runtime.setValue
 import androidx.lifecycle.ViewModel
+import androidx.lifecycle.viewModelScope
+import io.github.jenusdy.mars_photos.network.MarsApi
+import kotlinx.coroutines.launch
+import java.io.IOException
+
+sealed interface MarsUiState {
+    data class Success(val photos: String) : MarsUiState
+    object Error : MarsUiState
+    object Loading : MarsUiState
+}
 
 class HomeViewModel: ViewModel() {
 
-    var marsUiState: String by mutableStateOf("")
+    var marsUiState: MarsUiState by mutableStateOf(MarsUiState.Loading)
         private set
 
     init {
         getMarsPhotos()
     }
 
-    fun getMarsPhotos() {
-        marsUiState = "Set the Mars API Status Response Here"
+    private fun getMarsPhotos() {
+        viewModelScope.launch {
+            marsUiState = try {
+                val listResult = MarsApi.retrofitService.getPhotos()
+                MarsUiState.Success(listResult)
+            } catch (e: IOException) {
+                MarsUiState.Error
+            }
+        }
     }
 
 }
